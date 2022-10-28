39d83521be1da96c129fde3b5e940533350fe45a
Melquisedeque <melck_junior@hotmail.com>
2022-10-28 11:48:08 +0100

    Authentication redirect

--- src/components/CardLogin/CardLogin.tsx
+++ src/components/CardLogin/CardLogin.tsx
@@ -3,6 +3,7 @@ import { useForm } from 'react-hook-form';
 import './CardLogin.css';
 import { requestBackendLogin } from '../../util/requests';
 import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 
 type FormData = {
@@ -14,13 +15,16 @@ export default function CardLogin() {
 
     const [hasError, setHasError] = useState(false);
 
-    const { register, handleSubmit } = useForm<FormData>();
+    const { register, handleSubmit, formState : { errors} } = useForm<FormData>();
+
+    const history = useHistory();
 
     const onSubimit = (formData : FormData) => {
         requestBackendLogin(formData)
         .then(response => {
             setHasError(false);
             console.log('SUCESSO', response);
+            history.push('/movies');
 
         })
         .catch(error => {
@@ -42,19 +46,33 @@ export default function CardLogin() {
                 <form onSubmit={handleSubmit(onSubimit)} >
                     <div className="card-login-input">
                         <input 
-                        {...register("username")}
+                        {...register("username", {
+                            required: 'Campo obrigatório',
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: 'E-mail invalido'
+                              }
+                        })}
+                        className={`form-control base-input ${errors.username ? 'is-invalid' : ''}`}
                          type="text" 
-                         name='username' 
                          placeholder='Email' 
+                         name='username' 
                          />
                     </div>
-                    <div className="card-login-input">
+                    <div className="invalid-feedback primary d-block">{errors.username?.message}</div>
+                    <div className="card-login-input ">
                         <input 
-                        {...register("password")}
+                        {...register("password", {
+                            required: 'Campo obrigatório' 
+                        })}
+                        className={`form-control base-input ${errors.password ? 'is-invalid' : ''}`}
                         type="password" 
+                        placeholder='Senha'
                         name='password' 
-                        placeholder='Senha'/>
+                        />
                     </div>
+                    <div className="invalid-feedback d-block">{errors.password?.message}</div>
+
                         <Button text="fazer login"/>
                 </form>
         </div>
